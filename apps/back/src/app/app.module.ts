5064e7c99ea19fbdf41eeadbb23e772e6d1516fc
fhemery <f.hemery@hemit.fr>
2025-01-27 15:44:31 +0100

    refactor: cleanup useless controller

--- apps/back/src/app/app.module.ts
+++ apps/back/src/app/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PingModule } from '@owl-writey/ping';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 @Module({
   imports: [PingModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
