84f39ae14eb6e68fa1d1c813fc1407b69ee8056e
fhemery <f.hemery@hemit.fr>
2025-02-26 10:25:36 +0100

    chore: add logging

--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -5,11 +5,30 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { WinstonModule } from 'nest-winston';
+import { format, transports } from 'winston';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      level: 'info',
+      exitOnError: false,
+      transports: [
+        new transports.Console({
+          format: format.cli({ colors: { info: 'blue', error: 'red' } }),
+        }),
+        new transports.File({
+          dirname: process.env['OWL_LOG_PATH'] || './',
+          filename: 'owl-writey-back.log',
+          level: 'info',
+          format: format.simple(),
+          maxsize: 5 * 1024 * 1024,
+        }),
+      ],
+    }),
+  });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
