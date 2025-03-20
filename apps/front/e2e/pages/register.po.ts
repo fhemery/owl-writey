1be31025e2d4caf474b10921c9a83d609256f8a7
DorianeDTR <doriane.dutertre@oclock.school>
2025-03-20 12:19:25 +0100

    adding new registration test

--- apps/front/e2e/pages/register.po.ts
+++ apps/front/e2e/pages/register.po.ts
@@ -3,21 +3,21 @@ import { expect, Locator, Page } from '@playwright/test';
 import { Auth } from '../tools/auth';
 import { BasePo } from './base.po';
 
-export class SigninPo extends BasePo {
+export class RegisterPo extends BasePo {
   get pageLocator(): Locator {
     return this.page.locator('.register-page');
   }
   get nameInput(): Locator {
-    return this.pageLocator.locator('imput[name=name]');
+    return this.pageLocator.locator('input[name=name]');
   }
   get emailInput(): Locator {
-    return this.pageLocator.locator('imput[name=email]');
+    return this.pageLocator.locator('input[name=email]');
   }
   get passwordInput(): Locator {
-    return this.pageLocator.locator('imput[name=password]');
+    return this.pageLocator.locator('input[name=password]');
   }
   get repeatPasswordInput(): Locator {
-    return this.pageLocator.locator('imput[name=repeatPassword]');
+    return this.pageLocator.locator('input[name=repeatPassword]');
   }
   get submitButton(): Locator {
     return this.pageLocator.getByRole('button', {
@@ -31,5 +31,28 @@ export class SigninPo extends BasePo {
 
   async goTo(): Promise<void> {
     await this.page.goto('/register');
+  }
+
+  async shouldBeDisplayed(): Promise<void> {
+    await expect(this.pageLocator).toBeVisible();
+  }
+
+  async shouldDisplayForm(): Promise<void> {
+    await expect(this.nameInput).toBeVisible();
+    await expect(this.emailInput).toBeVisible();
+    await expect(this.passwordInput).toBeVisible();
+    await expect(this.repeatPasswordInput).toBeVisible();
+    await expect(this.submitButton).toBeVisible();
+  }
+
+  async registerAs(name: string, email: string, password: string, repeatedPassword: string): Promise<void> {
+    await this.nameInput.fill(name);
+    await this.emailInput.fill(email);
+    await this.passwordInput.fill(password);
+    await this.repeatPasswordInput.fill(repeatedPassword);
+    await Promise.all([
+      this.page.waitForURL('/dashboard'),
+      this.submitButton.click()
+    ]);
   }
 }
--- apps/front/e2e/specs/register.test.ts
+++ apps/front/e2e/specs/register.test.ts
@@ -1,2 +1,26 @@
 import { test } from '@playwright/test';
 
+import { RegisterPo } from '../pages/register.po';
+import { DashboardPo } from '../pages/dashboard.po';
+
+test.describe('Register page', () => {
+  let registerPo: RegisterPo;
+  let dashboardPo: DashboardPo;
+
+  test.beforeEach(async ({ page }) => {
+    registerPo = new RegisterPo(page);
+    dashboardPo = new DashboardPo(page);
+    await registerPo.goTo();
+  });
+
+  test('should be displayed', async() => {
+    await registerPo.shouldBeDisplayed();
+    await registerPo.shouldDisplayForm();
+  });
+
+  test('should redirect to the dashboard page on successful registration', async ({ page }) => {
+    await registerPo.registerAs('test12', 'testing@gmail.com', 'password', 'password');
+    // await page.waitForLoadState();
+    await dashboardPo.shouldBeDisplayed();
+  });
+})
