cec01ee8f9d85f67b8db0363cb6855d5c8e0d0f2
fhemery <f.hemery@hemit.fr>
2025-04-01 15:10:20 +0200

    feat(novels/front): updating text from scene is functional

--- libs/front/ui/novels/src/lib/pages/novel-scene/novel-scene-page.component.ts
+++ libs/front/ui/novels/src/lib/pages/novel-scene/novel-scene-page.component.ts
@@ -1,10 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { TextEditorComponent } from '@owl/front/ui/common';
+
+import { NovelSceneViewModel } from '../../model';
+import { NovelStore } from '../../services/novel.store';
 
 @Component({
   selector: 'owl-novel-scene-page',
-  imports: [CommonModule],
+  imports: [CommonModule, TextEditorComponent],
   templateUrl: './novel-scene-page.component.html',
   styleUrl: './novel-scene-page.component.scss',
 })
-export class NovelScenePageComponent {}
+export class NovelScenePageComponent {
+  readonly #novelStore = inject(NovelStore);
+  chapterId = input.required<string>();
+  sceneId = input.required<string>();
+
+  scene = computed(() => {
+    return this.#novelStore
+      .getNovel()
+      .chapters.find((c) => c.id === this.chapterId())
+      ?.scenes.find((s) => s.id === this.sceneId());
+  });
+
+  async updateContent(newText: string): Promise<void> {
+    const currentScene = this.scene();
+    if (!currentScene) {
+      return;
+    }
+    const scene = new NovelSceneViewModel(
+      currentScene.id,
+      currentScene.generalInfo,
+      newText
+    );
+    await this.#novelStore.updateScene(this.chapterId(), scene);
+  }
+}
