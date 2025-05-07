f448ec74e94a801bee1d8f28f21b75fec41497a2
fhemery <f.hemery@hemit.fr>
2025-05-07 14:26:30 +0200

    chore(front/common): catch up some tests on the editor component

---
+++ libs/front/ui/common/src/lib/components/text-editor/text-editor.component.spec.ts
@@ -0,0 +1,112 @@
+import { CommonModule } from '@angular/common';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestUtils } from '@owl/front/test-utils';
+import { Editor, NgxEditorModule } from 'ngx-editor';
+
+import { TextEditorComponent } from './text-editor.component';
+
+class EditorUtils {
+  constructor(
+    private readonly fixture: ComponentFixture<TextEditorComponent>
+  ) {}
+
+  typeText(text: string, clearCurrentContent = false): void {
+    const editorComponent = this.fixture.debugElement.query(
+      By.css('ngx-editor')
+    ).componentInstance['editor'] as Editor;
+    if (clearCurrentContent) {
+      editorComponent.setContent('');
+    }
+    let command = editorComponent.commands.focus('end');
+    let isFirstOperation = true;
+    text.split(`\n`).forEach((text) => {
+      if (isFirstOperation) {
+        isFirstOperation = false;
+      } else {
+        command = command.insertNewLine();
+      }
+      command.insertText(text);
+    });
+
+    command.exec();
+    this.fixture.detectChanges();
+  }
+}
+
+const editorSelector = '.ProseMirror.NgxEditor__Content';
+describe('TextEditorComponent', () => {
+  let component: TextEditorComponent;
+  let fixture: ComponentFixture<TextEditorComponent>;
+  let testUtils: TestUtils;
+  let lastEmittedText: string;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        TextEditorComponent,
+        CommonModule,
+        FormsModule,
+        NgxEditorModule,
+        BrowserAnimationsModule,
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TextEditorComponent);
+    component = fixture.componentInstance;
+    testUtils = new TestUtils(fixture);
+    component.update.subscribe((value) => {
+      lastEmittedText = value;
+    });
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    expect(testUtils.hasElement('.text-editor')).toBeTruthy();
+  });
+
+  describe('Input parameters', () => {
+    it('should update the text when currentContent changes', async () => {
+      const newContent = 'New content';
+      testUtils.setInput(() => component.currentContent, newContent, true);
+
+      await testUtils.waitStable();
+
+      expect(testUtils.getTextForElementAt(editorSelector)).toContain(
+        newContent
+      );
+    });
+  });
+
+  describe('When typing', () => {
+    it('should emit the text after a while', fakeAsync(async () => {
+      const editorUtils = new EditorUtils(fixture);
+      editorUtils.typeText('Something');
+
+      tick(2000);
+      await testUtils.waitStable();
+
+      expect(lastEmittedText).toContain('Something');
+    }));
+
+    it('should emit the text when focus is lost', async () => {
+      const editorUtils = new EditorUtils(fixture);
+      editorUtils.typeText('Something');
+      testUtils.dispatchEvent('blur', editorSelector);
+
+      fixture.detectChanges();
+      await testUtils.waitStable();
+
+      expect(lastEmittedText).toContain('Something');
+    });
+  });
+});
