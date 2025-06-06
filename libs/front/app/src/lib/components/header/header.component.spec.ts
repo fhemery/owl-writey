abc1d2ed022e49b2d05e71dcea7074f7f096eed1
fhemery <f.hemery@hemit.fr>
2025-06-06 14:05:50 +0200

    chore(front): add test for the header menu

--- libs/front/app/src/lib/components/header/header.component.spec.ts
+++ libs/front/app/src/lib/components/header/header.component.spec.ts
@@ -1,7 +1,10 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 import { signal, WritableSignal } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatMenuHarness } from '@angular/material/menu/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Router } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { AuthService, UserProfile, UserService } from '@owl/front/auth';
 import { TestUtils } from '@owl/front/test-utils';
@@ -14,6 +17,7 @@ describe('HeaderComponent', () => {
   let mockUserService: Partial<AuthService>;
   let userSignal: WritableSignal<UserProfile | null>;
   let testUtils: TestUtils;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     userSignal = signal(null);
@@ -37,6 +41,7 @@ describe('HeaderComponent', () => {
     component = fixture.componentInstance;
     testUtils = new TestUtils(fixture);
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create', () => {
@@ -73,8 +78,46 @@ describe('HeaderComponent', () => {
       fixture.detectChanges();
     });
 
-    it('should display the user menu', () => {
+    it('should display the user menu', async () => {
       expect(testUtils.hasElement('.header__user-menu')).toBeTruthy();
+
+      const [firstMenu] = await loader.getAllHarnesses(MatMenuHarness);
+      expect(await firstMenu.getTriggerText()).toContain('Alice');
+    });
+
+    it('should display dashboard and logout buttons when opening the menu', async () => {
+      const [menu] = await loader.getAllHarnesses(MatMenuHarness);
+      await menu.open();
+
+      const items = await menu.getItems();
+      expect(items.length).toBe(2);
+
+      expect(await items[0].getText()).toContain('home.header.dashboardLink');
+      expect(await items[1].getText()).toContain('home.header.logoutLink');
+    });
+
+    it('should redirect to dashboard when clicking on dashboard link', async () => {
+      const [menu] = await loader.getAllHarnesses(MatMenuHarness);
+      await menu.open();
+
+      const items = await menu.getItems();
+      await items[0].click();
+      await testUtils.waitStable();
+
+      const router = TestBed.inject(Router);
+      expect(router.url).toBe('/dashboard');
+    });
+
+    it('should redirect to logout when clicking on logout link', async () => {
+      const [menu] = await loader.getAllHarnesses(MatMenuHarness);
+      await menu.open();
+
+      const items = await menu.getItems();
+      await items[1].click();
+      await testUtils.waitStable();
+
+      const router = TestBed.inject(Router);
+      expect(router.url).toBe('/login/logout');
     });
 
     it('should not display login and register buttons', () => {
