66bbf0290e5107e83675f3f675eb7c9383753850
DorianeDTR <doriane.cc.dutertre@gmail.com>
2025-12-05 10:06:38 +0100

    updating e2e API exercise test

--- apps/front/e2e/steps/e2e.ts
+++ apps/front/e2e/steps/e2e.ts
@@ -13,12 +13,6 @@ Given('I am on the owl-writey homepage', async ({ page, homePo }: AllFixtures) =
     );
 
     await homePo.goTo(); 
-
-    const configResponse = await getConfigResponse;
-
-    console.log(`URL de la requête API: ${configResponse.url()}`);
-    console.log(`Méthode de la requête: ${configResponse.request().method()}`);
-    console.log(`Statut de la réponse: ${configResponse.status()}`);
 });
 
 When ('I log as a known user for creating an exercise', async ({ page, loginPo, dashboardPo, exerciseCreatePo } : AllFixtures) => {
@@ -39,30 +33,12 @@ When ('I log as a known user for creating an exercise', async ({ page, loginPo, 
     );
     await loginPo.logAs('bob@hemit.fr', 'Test123!');
 
-    const userResponse = await getUsersResponse;
-    const eventsResponse = await getUsersEventsResponse;
-
-    console.log(`URL de la requête API: ${userResponse.url()}`);
-    console.log(`Méthode de la requête: ${userResponse.request().method()}`);
-    console.log(`Statut de la réponse: ${userResponse.status()}`);
-    
-    console.log(`URL de la requête API: ${eventsResponse.url()}`);
-    console.log(`Méthode de la requête: ${eventsResponse.request().method()}`);
-    console.log(`Statut de la réponse: ${eventsResponse.status()}`);
-
     const getExercisesDisplayResponse = page.waitForResponse(response => 
         response.url().includes('/api/exercises') && 
         response.request().method() === 'GET' && 
         response.status() === 200 
     );
     await dashboardPo.shouldBeDisplayed();
-
-    const exercisesDisplayResponse = await getExercisesDisplayResponse;
-
-    console.log(`URL de la requête API: ${exercisesDisplayResponse.url()}`);
-    console.log(`Méthode de la requête: ${exercisesDisplayResponse.request().method()}`);
-    console.log(`Statut de la réponse: ${exercisesDisplayResponse.status()}`);
-
     await dashboardPo.createNewExercise();
 
     const getCreateExerciseResponse = page.waitForResponse(response => 
@@ -73,10 +49,6 @@ When ('I log as a known user for creating an exercise', async ({ page, loginPo, 
     await exerciseCreatePo.createdAs('Test d\'exercice owl-writey', '4', '5 minutes', '4', '5', 'Ceci est un test pour valider ou non le bon fonctionnement du formulaire');
 
     const exerciseCreationResponse = await getCreateExerciseResponse;
-
-    console.log(`URL de la requête API: ${exerciseCreationResponse.url()}`);
-    console.log(`Méthode de la requête: ${exerciseCreationResponse.request().method()}`);
-    console.log(`Statut de la réponse: ${exerciseCreationResponse.status()}`);
 });
 Then('I can try to take a turn on it, submit, cancel my turn, finish the exercise', async ({ page, exerciseCurrentPo, exquisiteCorpsePo, confirmDialogPo } : AllFixtures) => {
     const getExerciseDisplayedResponse = page.waitForResponse(response => 
@@ -86,12 +58,6 @@ Then('I can try to take a turn on it, submit, cancel my turn, finish the exercis
     );
     await exerciseCurrentPo.shouldBeDisplayed();
 
-    const displayedExerciseresponse = await getExerciseDisplayedResponse;
-
-    console.log(`URL de la requête API: ${displayedExerciseresponse.url()}`);
-    console.log(`Méthode de la requête: ${displayedExerciseresponse.request().method()}`);
-    console.log(`Statut de la réponse: ${displayedExerciseresponse.status()}`);
-
     const getTakeTurnResponse = page.waitForResponse(response => 
         response.url().includes('/api/exquisite-corpse/') && 
         response.url().includes('/take-turn') &&
@@ -99,13 +65,6 @@ Then('I can try to take a turn on it, submit, cancel my turn, finish the exercis
         response.status() === 204 
     );
     await exquisiteCorpsePo.takePartToExercise();
-
-    const takeTurnResponse = await getTakeTurnResponse;
-
-    console.log(`URL de la requête API: ${takeTurnResponse.url()}`);
-    console.log(`Méthode de la requête: ${takeTurnResponse.request().method()}`);
-    console.log(`Statut de la réponse: ${takeTurnResponse.status()}`);
-
     await exquisiteCorpsePo.filledWith('Mon tour est arrivé');
 
     const getSubmitTurnResponse = page.waitForResponse(response => 
@@ -116,13 +75,6 @@ Then('I can try to take a turn on it, submit, cancel my turn, finish the exercis
     );
 
     await exquisiteCorpsePo.submitText();
-
-    const submittedResponse = await getSubmitTurnResponse;
-
-    console.log(`URL de la requête API: ${submittedResponse.url()}`);
-    console.log(`Méthode de la requête: ${submittedResponse.request().method()}`);
-    console.log(`Statut de la réponse: ${submittedResponse.status()}`);
-
     await exquisiteCorpsePo.takePartToExercise();
 
      const getCancelTurnResponse = page.waitForResponse(response => 
@@ -133,13 +85,6 @@ Then('I can try to take a turn on it, submit, cancel my turn, finish the exercis
     );
 
     await exquisiteCorpsePo.giveUpTurn();
-
-    const cancelledResponse = await getCancelTurnResponse;
-
-    console.log(`URL de la requête API: ${cancelledResponse.url()}`);
-    console.log(`Méthode de la requête: ${cancelledResponse.request().method()}`);
-    console.log(`Statut de la réponse: ${cancelledResponse.status()}`);
-
     await exerciseCurrentPo.finishExerciseAction();
 
     const getFinishExerciseResponse = page.waitForResponse(response => 
@@ -150,12 +95,6 @@ Then('I can try to take a turn on it, submit, cancel my turn, finish the exercis
     );
 
     await confirmDialogPo.confirmDeleteAction();
-
-    const finishedExerciseResponse = await getFinishExerciseResponse;
-
-    console.log(`URL de la requête API: ${finishedExerciseResponse.url()}`);
-    console.log(`Méthode de la requête: ${finishedExerciseResponse.request().method()}`);
-    console.log(`Statut de la réponse: ${finishedExerciseResponse.status()}`);
 });
 Then('I finally delete the exercise', async ({ page, exerciseCurrentPo, confirmDialogPo, dashboardPo, exerciseCardPo } : AllFixtures) => {
     await dashboardPo.shouldBeDisplayed();
@@ -170,13 +109,6 @@ Then('I finally delete the exercise', async ({ page, exerciseCurrentPo, confirmD
     );
 
     await exerciseCardPo.displayExerciseCard('Test d\'exercice owl-writey');
-
-    const eventsResponse = await getExercisesEventsResponse;
-
-    console.log(`URL de la requête API: ${eventsResponse.url()}`);
-    console.log(`Méthode de la requête: ${eventsResponse.request().method()}`);
-    console.log(`Statut de la réponse: ${eventsResponse.status()}`);
-    
     await exerciseCurrentPo.shouldBeDisplayed();
     await exerciseCurrentPo.deleteExerciseAction();
 
@@ -187,12 +119,5 @@ Then('I finally delete the exercise', async ({ page, exerciseCurrentPo, confirmD
     );
 
     await confirmDialogPo.confirmDeleteAction();
-
-    const deltedResponse = await getExerciseDeleteResponse;
-
-    console.log(`URL de la requête API: ${deltedResponse.url()}`);
-    console.log(`Méthode de la requête: ${deltedResponse.request().method()}`);
-    console.log(`Statut de la réponse: ${deltedResponse.status()}`);
-
     await dashboardPo.shouldBeDisplayed();
 });
